22b7e4df41b13aeae0d73e2365a7f585bcb689d9
sahilsheshank <138553429+sahilsheshank@users.noreply.github.com>
2024-04-07 20:32:05 +0530

    Update Home.jsx

--- ecommerce/src/pages/Home/Home.jsx
+++ ecommerce/src/pages/Home/Home.jsx
@@ -10,7 +10,7 @@ function Home({userName}) {
             <img src="ecommerce-banner.jpg" alt="image" className=' opacity-70 top- w-full object-cover object-center h-[100vh]  ' />
           </div>
           <div className='absolute top-[40%] left-[20%]'>
-            <h1 className='text-6xl font-bold text-black'>Welcome To Men's {userName}<span className='text-red-600'>Club</span></h1>
+            <h1 className='text-6xl font-bold text-black'>Welcome To Our Shopping hub <span className='text-red-600'>{userName}</span></h1>
             <h2 className='text-2xl font-bold text-gray-700 '>All About Men's Apparel</h2>
           </div>
         </div>
